96b2e237a086e1263a1d8956c59fc085f8df97b6
tetyanamartyniuk <145157063+tetyanamartyniuk@users.noreply.github.com>
2025-04-01 00:09:27 +0300

    незрозуміло, чи все я додаю

--- lab2/server.js
+++ lab2/server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-// Встановлюємо EJS як шаблонізатор
 app.set("view engine", "ejs");
 
-// Маршрут для рендерингу сторінки
 
     app.get("/Vova", (req, res) => {
     res.render("index", { name: "Вова", img: "/images/V.jpg", description:"Maecenas at orci nec sem malesuada hendrerit vitae eu tellus. Morbi nec nisl libero. Quisque quam ligula, sodales ut velit et, malesuada sollicitudin felis. Fusce sed pretium arcu, eu convallis enim. Mauris vel ante sed felis tincidunt eleifend. Nunc ornare viverra magna. Sed sit amet mollis ligula."});
@@ -25,7 +23,6 @@ app.get("/", (req, res) => {
 
 });
 
-// Запуск сервера
-app.listen(3000, () => {
-    console.log("Сервер запущено на http://localhost:3000");
+app.listen(3001, () => {
+    console.log("Сервер запущено на http://localhost:3001");
 });

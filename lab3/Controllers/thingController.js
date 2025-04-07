3f819027bb58130e9b9b127829e7a75eb9fcb965
Cucumbao <144848280+Cucumbao@users.noreply.github.com>
2025-04-07 13:06:03 +0300

    динамічна сторінка

--- lab3/Controllers/thingController.js
+++ lab3/Controllers/thingController.js
@@ -4,5 +4,19 @@ export const getThings = (req, res) =>{
     const things = getAllThings()
     res.render("allThingsPage", { things });
 };
+export const getThingfor = (req, res) =>{
+    const things = getAllThings()
+    const thing = things.find(t => t.id ===  Number(req.params.id));
+    if (thing) {
+        res.render("createPage", {
+            name: thing.name,
+            place: thing.place,
+            email: thing.email,
+            phone: thing.phone
+        });
+    } else {
+        res.status(404).send("Предмет не знайдено");
+    }
+};
 
-export default{getThings}
+export default{getThings, getThingfor}
--- lab3/Router.js
+++ lab3/Router.js
@@ -5,25 +5,11 @@ import {deleteThing} from "./Controllers/deleteThingController.js"
 import {findThing} from "./Controllers/findThingController.js";
 const app = express();
 const router = express.Router();
-
 router.post("/addThing", addThing);
 router.get("/allThings", thingController.getThings)
+router.get("/:id", thingController.getThingfor);
 router.delete("/deleteThing", deleteThing )
 router.post("/findThing", findThing)
 app.use("/users", router)
 
 export default router
-
-
-
-app.get("/iphone",(req, res)=>{
-    res.render("createPage", {id:1, name:"iphone 24 pro excel", img:"cz", description:"Айфон найновішої моделі, розроблений за підтримки Майкрософт. Валявся на поляні під шарою, синенький як студенти"})
-});
-
-app.get("/knife",(req, res)=>{
-    res.render("createPage", {id:2, name:"ніж", img:"xzc", description:"Знайшли на поляні, в синенькому студенті, під шарою."})
-});
-
-app.get("/boat",(req, res)=>{
-    res.render("createPage", {id:3, name:"лодка", img:"sc", description:"Знайшли на зупинці швидкісного трамваю, на коліях. Розмір 2*3м, без моторчика"})
-});
